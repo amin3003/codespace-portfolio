434586458ca0ff79acf396fed269f62aa8604650
amin <aminazimi1701@gmail.com>
2023-12-18 23:27:19 +0300

    added suport multi lang

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Image from 'next/image';
-
-export default async function Home() {
-	return <div className="flex min-h-screen justify-between p-24 gap-2 flex-col"></div>;
-}
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,11 @@
+
+
+// export default async function Home() {
+// 	return <div className="flex min-h-screen justify-between p-24 gap-2 flex-col"></div>;
+// }
+import {useTranslations} from 'next-intl';
+ 
+export default function Index() {
+  const t = useTranslations('Index');
+  return <h1>{t('title')}</h1>;
+}
