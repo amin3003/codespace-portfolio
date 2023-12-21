d445abecf6ce81ee5fca4d1adfc94aaae4b0b390
amin <aminazimi1701@gmail.com>
2023-12-21 18:08:35 +0300

    manage translate

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,46 +1,27 @@
-// import React from 'react';
-// import type { Metadata } from 'next';
-// import { Inter } from 'next/font/google';
-// import './globals.css';
-// import 'bootstrap-icons/font/bootstrap-icons.css';
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import AppHeader from "@c/Header/AppHeader";
+import Footer from "@c/Footer/Footer";
+import { notFound } from "next/navigation";
 
-// import AppHeader from '@c/Header/AppHeader';
-// import Footer from '@c/Footer/Footer';
-
-// const inter = Inter({ subsets: ['latin'] });
-
-// export const metadata: Metadata = {
-// 	title: 'Redronic',
-// 	description: '',
-// };
-
-// export default function RootLayout({ children }: { children: React.ReactNode }) {
-// 	return (
-// 		<html lang="en" data-theme="dark">
-// 			<body className={inter.className}>
-// 				<AppHeader />
-// 				<main className="flex-1 overflow-y-auto min-h-screen">{children}</main>
-// 				<Footer />
-// 			</body>
-// 		</html>
-// 	);
-// }
-
-
-
-
-import {notFound} from 'next/navigation';
- 
 // Can be imported from a shared config
-const locales = ['en', 'tr'];
- 
-export default function LocaleLayout({children, params: {locale}}) {
+const locales = ["en", "tr"];
+
+export default function LocaleLayout({ children, params: { locale } }) {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
- 
+
   return (
     <html lang={locale}>
-      <body>{children}</body>
+      <body>
+        <AppHeader />
+        <main className="flex-1 overflow-y-auto min-h-screen">{children}</main>
+        <Footer />
+        <script src="https://unpkg.com/flowbite@1.5.1/dist/flowbite.js"></script>
+      </body>
     </html>
   );
 }
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,12 @@
-
-
 // export default async function Home() {
 // 	return <div className="flex min-h-screen justify-between p-24 gap-2 flex-col"></div>;
 // }
-import {useTranslations} from 'next-intl';
- 
+import { useTranslations } from "next-intl";
+
 export default function Index() {
-  const t = useTranslations('Index');
-  return <h1>{t('title')}</h1>;
+  const t = useTranslations("Index");
+  return (
+    <div className="flex min-h-screen justify-between p-24 gap-2 flex-col"><h1>{t('title')}</h1></div>
+  );
+  // return <h1>{t('title')}</h1>;
 }
