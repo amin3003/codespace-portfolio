2397886a66fb425655382729a15bedf85b07a1d0
amin <aminazimi1701@gmail.com>
2023-12-30 20:37:28 +0300

    animation-v01

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,7 +1,7 @@
 import { useTranslations } from 'next-intl';
 
 export default function About() {
-	const t = useTranslations('basic');
+	const t = useTranslations('navbar');
 	return (
 		<div className="flex min-h-screen justify-between p-24 gap-2 flex-col">
 			<h1>{t('about')}</h1>
--- src/app/[locale]/portfolio/page.tsx
+++ src/app/[locale]/portfolio/page.tsx
@@ -1,10 +1,13 @@
-import { useTranslations } from 'next-intl';
-
+import { useTranslations } from "next-intl";
+import Spotlight from "../../../components/animations/Spotlight";
+import '../../../components/animations/styles.css'
 export default function Portfolio() {
-	const t = useTranslations('basic');
-	return (
-		<div className="flex min-h-screen justify-between p-24 gap-2 flex-col">
-			<h1>{t('portfolio')}</h1>
-		</div>
-	);
+  const t = useTranslations("navbar");
+  return (
+    // <div className="flex min-h-screen justify-between p-24 gap-2 flex-col">
+    // </div>
+    <>
+      <Spotlight />
+    </>
+  );
 }
