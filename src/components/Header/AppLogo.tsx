51a1cda7cf3f1246cbba777f089cc7621e39f2b4
Azrideus <22293394+Azrideus@users.noreply.github.com>
2023-12-31 19:02:54 +0300

    fix: header for mobile

--- src/components/Header/AppLogo.tsx
+++ src/components/Header/AppLogo.tsx
@@ -1,20 +1,20 @@
 import Image from 'next/image';
-  import Link from '@/navigation';
+import Link from '@/navigation';
 
-	export default function AppLogo(props: any) {
-		return (
-			<Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-				{/* <Image src="" alt="chiztech logo" /> */}
-				{props.text && (
-					<div>
-						<span className="self-center text-2xl font-semibold whitespace-nowrap text-orange-400">
-							Chiz
-						</span>
-						<span className="self-center text-2xl font-semibold whitespace-nowrap text-white ml-1">
-							|Tech
-						</span>
-					</div>
-				)}
-			</Link>
-		);
-	}
+export default function AppLogo(props: { className?: string; text: boolean }) {
+	return (
+		<Link href="/" className={'flex items-center ' + props.className}>
+			{/* <Image src="" alt="chiztech logo" /> */}
+			{props.text && (
+				<div>
+					<span className="self-center text-2xl font-semibold whitespace-nowrap text-orange-400">
+						Chiz
+					</span>
+					<span className="self-center text-2xl font-semibold whitespace-nowrap text-white ml-1">
+						|Tech
+					</span>
+				</div>
+			)}
+		</Link>
+	);
+}
