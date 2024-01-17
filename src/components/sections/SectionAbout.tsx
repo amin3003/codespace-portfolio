dfb6bdc482f7399f7938e2002480294cdc535d65
amin <aminazimi1701@gmail.com>
2024-01-17 18:54:52 +0300

    add carousel

--- src/components/sections/SectionAbout.tsx
+++ src/components/sections/SectionAbout.tsx
@@ -1,53 +1,90 @@
-import { Text } from '@c/shared/Text';
-import { math_lerp } from '@azrico/math';
-import { Block, useBlock, BlockProperties } from '@c/shared/Blocks';
-import { Html } from '@react-three/drei';
-import { sectionProps } from './types';
+import { Text } from "@c/shared/Text";
+import { math_lerp } from "@azrico/math";
+import { Block, useBlock, BlockProperties } from "@c/shared/Blocks";
+import { Html } from "@react-three/drei";
+import { sectionProps } from "./types";
 
 export default function SectionAbout(props: sectionProps) {
-	const team = [
-		{
-			img: '',
-			name: 'Ayhan',
-			position: 'Back-end',
-			desc: 'It by the readable at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution',
-			social: {
-				gitHub: '',
-				instagram: '',
-				telegram: '',
-			},
-		},
-	];
-	return (
-		<Block {...props} factor={2.5}>
-			<Text color="white" size={props.block.wModifier}>
-				{'Chiz ' + '<About/>'}
-			</Text>
-			<Html
-				className="flex flex-row gap-4 p-4 justify-between"
-				style={{ width: props.block.canvasWidth }}
-				position={[-props.block.canvasWidth / 2, -90, 0]}
-			>
-				{team.map((r, index) => {
-					return (
-						<div key={index} className="card w-56 bg-base-100 shadow-xl">
-							<div className="card-body content-center justify-center ">
-								<div className="avatar justify-center">
-									<div className="w-24 rounded-full ">
-										<img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-									</div>
-								</div>
-								<h2 className="card-title justify-center">{r.name}</h2>
-								<h3 className="flex justify-center text-secondary">{r.position}</h3>
-								<p className="flex text-justify text-sm">{r.desc}</p>
-								<div className="card-actions justify-center">
-								
-								</div>
-							</div>
-						</div>
-					);
-				})}
-			</Html>
-		</Block>
-	);
+  const team = [
+    {
+      img: "",
+      name: "Ayhan",
+      position: "Back-end Developer",
+      desc: "It by the readable at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution",
+      social: {
+        gitHub: "",
+        instagram: "",
+        telegram: "",
+      },
+    },
+    {
+      img: "",
+      name: "Emin",
+      position: "Front-end Developer",
+      desc: "It by the readable at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution",
+      social: {
+        gitHub: "",
+        instagram: "",
+        telegram: "",
+      },
+    },
+    {
+      img: "",
+      name: "Reza",
+      position: "UI/UX Designer ",
+      desc: "It by the readable at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution",
+      social: {
+        gitHub: "",
+        instagram: "",
+        telegram: "",
+      },
+    },
+  ];
+  return (
+    <Block {...props} factor={1.8}>
+      <Text color="white" size={props.block.wModifier}>
+        {"Chiz " + "<About/>"}
+      </Text>
+      <Html
+        className="flex flex-col  p-4 "
+        style={{ width: props.block.canvasWidth }}
+        position={[-props.block.canvasWidth / 2, -90, 0]}
+      >
+        <div className="flex gap-4 carousel carousel-center">
+          {team.map((r, index) => {
+            return (
+              <div
+                key={index}
+				id={`team${index}`}
+                className="carousel-item card w-[300px] h-[350px] bg-base-100 shadow-xl"
+              >
+                <div className="card-body content-center justify-center ">
+                  <div className="avatar justify-center">
+                    <div className="w-24 rounded-full ">
+                      <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
+                  </div>
+                  <h2 className="card-title justify-center">{r.name}</h2>
+                  <h3 className="flex justify-center text-secondary">
+                    {r.position}
+                  </h3>
+                  <p className="flex text-justify text-sm">{r.desc}</p>
+                  <div className="card-actions justify-center"></div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex justify-center w-full py-2 gap-2">
+          {team.map((r, i) => {
+            return (
+              <a href={`#team${i}`} className="btn btn-xs">
+                {i + 1}
+              </a>
+            );
+          })}
+        </div>
+      </Html>
+    </Block>
+  );
 }
--- src/components/shared/Blocks.tsx
+++ src/components/shared/Blocks.tsx
@@ -1,80 +1,90 @@
-"use client"
-import React, { createContext, useRef, useContext } from "react"
-import { useFrame, useThree } from "@react-three/fiber"
+"use client";
+import React, { createContext, useRef, useContext } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
 import { math_lerp } from "@azrico/math";
-import state from "@c/shared/Store"
-import { Color } from 'three';
-import { Html, Plane, Sphere } from '@react-three/drei';
+import state from "@c/shared/Store";
+import { Color } from "three";
+import { Html, Plane, Sphere } from "@react-three/drei";
 
 const offsetContext = createContext(0);
 
 function Block({ children, offset, factor, ...props }: any) {
-	const { offset: parentOffset, sectionHeight } = useBlock();
-	const ref = useRef<any>();
+  const { offset: parentOffset, sectionHeight, fModifier } = useBlock();
+  const ref = useRef<any>();
 
-	offset = props.offset ?? props.index ?? parentOffset;
+  offset = props.offset ?? props.index ?? parentOffset;
 
-	useFrame(() => {
-		const curY = ref.current?.position.y;
-		const curTop = state.top.current;
-		//smooth movement of each section
-		ref.current.position.y = math_lerp(curY, (curTop / state.zoom) * factor, 0.1);
-	});
-	return (
-		<offsetContext.Provider value={offset}>
-			<group {...props} position={[0, -sectionHeight * offset * factor, 0]}>
-				<group ref={ref}>{children}</group>
-			</group>
-		</offsetContext.Provider>
-	);
+  useFrame(() => {
+    const curY = ref.current?.position.y;
+    const curTop = state.top.current;
+    //smooth movement of each section
+    ref.current.position.y = math_lerp(
+      curY,
+      (curTop / state.zoom) * factor,
+      0.1
+    );
+  });
+  return (
+    <offsetContext.Provider value={offset}>
+      <group
+        {...props}
+        position={[0, -sectionHeight * offset * factor * fModifier, 0]}
+      >
+        <group ref={ref}>{children}</group>
+      </group>
+    </offsetContext.Provider>
+  );
 }
 
 export interface BlockProperties {
-	viewport: { width: number; height: number };
-	offset: number;
-	viewportWidth: number;
-	viewportHeight: number;
-	canvasWidth: number;
-	canvasHeight: number;
-	mobile: boolean;
-	margin: number;
-	contentMaxWidth: number;
-	sectionHeight: number;
-	offsetFactor: number;
-	wModifier: number;
+  viewport: { width: number; height: number };
+  offset: number;
+  viewportWidth: number;
+  viewportHeight: number;
+  canvasWidth: number;
+  canvasHeight: number;
+  mobile: boolean;
+  margin: number;
+  contentMaxWidth: number;
+  sectionHeight: number;
+  offsetFactor: number;
+  wModifier: number;
+  fModifier: number;
 }
 
 function useBlock(): BlockProperties {
-	const { sections, pages, zoom } = state;
-	const { size, viewport } = useThree();
-	const offset = useContext(offsetContext);
-	const viewportWidth = viewport.width * zoom;
-	const viewportHeight = viewport.height * zoom;
-	const canvasWidth = viewportWidth / zoom;
-	const canvasHeight = viewportHeight / zoom;
+  const { sections, pages, zoom } = state;
+  const { size, viewport } = useThree();
+  const offset = useContext(offsetContext);
+  const viewportWidth = viewport.width * zoom;
+  const viewportHeight = viewport.height * zoom;
+  const canvasWidth = viewportWidth / zoom;
+  const canvasHeight = viewportHeight / zoom;
 
-	const mobile = size.width < 700;
-	const margin = canvasWidth * (mobile ? 0.2 : 0.1);
-	const wModifier = mobile ? canvasWidth * 1.5 : canvasWidth;
+  const mobile = size.width < 700;
+  const margin = canvasWidth * (mobile ? 0.2 : 0.1);
+  const wModifier = mobile ? canvasWidth * 1.5 : canvasWidth;
+  const fModifier = mobile ?  1 : 0.9;
 
-	const contentMaxWidth = canvasWidth * (mobile ? 0.8 : 0.6);
-	const sectionHeight = canvasHeight * ((pages - 1) / (sections - 1));
+  const contentMaxWidth = canvasWidth * (mobile ? 0.8 : 0.6);
+  const sectionHeight = canvasHeight * ((pages - 1) / (sections - 1));
 
-	const offsetFactor = (offset + 1.0) / sections;
-	return {
-		viewport,
-		offset,
-		viewportWidth,
-		viewportHeight,
-		canvasWidth,
-		canvasHeight,
-		mobile,
-		margin,
-		contentMaxWidth,
-		sectionHeight,
-		offsetFactor,
-		wModifier,
-	};
+  const offsetFactor = (offset + 1.0) / sections;
+  return {
+    viewport,
+    offset,
+    viewportWidth,
+    viewportHeight,
+    canvasWidth,
+    canvasHeight,
+    mobile,
+    margin,
+    contentMaxWidth,
+    sectionHeight,
+    offsetFactor,
+    wModifier,
+    fModifier,
+  };
 }
 
-export { Block, useBlock }
+export { Block, useBlock };
