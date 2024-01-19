9280633760baf5460a9238cf7be34b3027f3df56
amin <aminazimi1701@gmail.com>
2024-01-19 17:43:29 +0300

    slider v-02

--- src/components/swiper/ServicesSlider.tsx
+++ src/components/swiper/ServicesSlider.tsx
@@ -13,15 +13,15 @@ export default function ServicesSlider(props: any) {
       pagination={{ clickable: true }}
       breakpoints={{
         640: {
-          slidesPerView: 2,
+          slidesPerView: 1,
           spaceBetween: 20,
         },
         768: {
-          slidesPerView: 3,
+          slidesPerView: 2,
           spaceBetween: 40,
         },
         1024: {
-          slidesPerView: 5,
+          slidesPerView: 3, //this value should be the same as the number of all boxes
           spaceBetween: 50,
         },
       }}
@@ -40,11 +40,11 @@ export default function ServicesSlider(props: any) {
           className="bg-base-100"
           style={{ borderRadius: "0px", background: "none" }}
         >
-          <div className="card bg-base-100">
+          <div className="flex justify-center items-center card bg-base-100 h-[400px] gap-10">
             <div className="card-body">
-              <h2 className="card-title">{r.name}</h2>
-              <p>{r.desc}</p>
-              <div className="card-actions justify-end">
+              <h2 className="card-title text-center">{r.name}</h2>
+              <p className="text-center text-[16 px]">{r.desc}</p>
+              <div className="card-actions justify-center">
                 <button className="btn btn-primary">Buy Now</button>
               </div>
             </div>
