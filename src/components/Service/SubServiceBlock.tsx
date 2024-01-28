ccb9f1c20c9ce5e2096ed4e2cc23f8d4249552ba
Azrideus <22293394+Azrideus@users.noreply.github.com>
2024-01-28 16:25:31 +0300

    using css variables for subservice color

--- src/components/Service/SubServiceBlock.tsx
+++ src/components/Service/SubServiceBlock.tsx
@@ -1,32 +1,25 @@
-import { SubService } from '@/data/Service';
+import { Feature, SubService } from '@/data/Service';
 import Link from '@src/navigation';
 export default function SubServiceBlock(props: {
 	index: number;
 	subservice: SubService;
 }) {
 	const r = props.subservice;
+	const subservice_style = { '--subservicecolor': r.color } as React.CSSProperties;
 
-	const shadowVariants = [
-		'border-red-300',
-		'border-blue-300',
-		'border-pink-300',
-		'border-green-300',
-	];
 	return (
-		<div className="indicator">
-			<div
-				className={`bg-base-100 flex border-t-4 w-[260px] ${shadowVariants[props.index]}`}
-			>
+		<div className="indicator" style={subservice_style}>
+			<div className={`bg-base-100 flex border-t-4 w-[260px] border-[--subservicecolor]`}>
 				<div className="card-body content-center flex">
 					<div className="flex-1">
 						<b className="card-title text-center justify-center">{r.name}</b>
 						<p className="text-center justify-center">{r.price}</p>
 						<ul className="pt-2 gap-2">
-							{r.features.map((feature: string, i: number) => {
+							{r.features.map((feature: Feature, i: number) => {
 								return (
 									<li key={i}>
 										<i className="bi bi-check"></i>
-										<b>{feature}</b>
+										<b>{feature.title}</b>
 									</li>
 								);
 							})}
