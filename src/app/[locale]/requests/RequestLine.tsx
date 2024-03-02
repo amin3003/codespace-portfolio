066b383929a741bfc244505756de5bccb014d8f9
Azrideus <22293394+Azrideus@users.noreply.github.com>
2024-03-03 00:25:53 +0300

    show serviceid in requestLine

--- src/app/[locale]/requests/RequestLine.tsx
+++ src/app/[locale]/requests/RequestLine.tsx
@@ -8,6 +8,7 @@ export default function RequestLine({ index, item }: any) {
 			{index != 0 && <div className="divider" />}
 			<div className="flex flex-row gap-4">
 				<b className="px-2">{Number(index + 1)}</b>
+				<i className="hidden md:flex">{item.serviceid}</i>
 				<div className="flex-[5]">{item.desc}</div>
 				<i>{date_short(item._created_date)}</i>
 				<b className="uppercase">{item.status || 'Pending'}</b>
